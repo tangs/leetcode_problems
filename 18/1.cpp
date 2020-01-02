1d48c3a57094d8c5fc1f03352dfa8425f006e651
tangs <tangsong1988kaka@gmail.com>
2020-01-02 11:04:04 +0800

    optimize 18 problem.

--- 18/1.cpp
+++ 18/1.cpp
@@ -5,30 +5,6 @@
 using namespace std;
 
 class Solution {
-    inline int calcsum(vector<int>& nums, int poses[4]) {
-        int sum = 0;
-        for (int i = 0; i < 4; ++i) sum += nums[poses[i]];
-        return sum;
-    }
-
-    inline int getlessIdx(vector<int>& nums, int poses[4], int target) {
-        for (int i = 0; i < 4; ++i) if (nums[poses[i]] < target) return i;
-        return -1;
-    }
-
-    inline int getmoreIdx(vector<int>& nums, int poses[4], int target) {
-        for (int i = 0; i < 4; ++i) if (nums[poses[i]] > target) return i;
-        return -1;
-    }
-
-    inline int getNextIdx(vector<int>& nums, int poses[4], int idx, bool ignoreNext) {
-        int curIdx = poses[idx];
-        int cur = nums[curIdx];
-        int max = nums.size();
-        if (!ignoreNext && idx < 3) max = poses[idx + 1];
-        for (int i = curIdx + 1; i < max; ++i) if (nums[i] != cur) return i;
-        return -1;
-    }
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         vector<vector<int>> ret;
@@ -37,14 +13,22 @@ public:
         sort(nums.begin(), nums.end());
         
         int len = size - 3;
-        for (int i = 0; i < size - 3;) {
-            int len1 = len + 1;
+        for (int i = 0; i < len;) {
             int value1 = nums[i];
+            if (value1 > 0 && value1 >= target) break;
+            int len1 = len + 1;
+            if (value1 + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) goto endi;
+            if (value1 + nums[len] + nums[len + 1] + nums[len + 2] < target) goto endi;
             for (int j = i + 1; j < len1;) {                
                 int value2 = nums[j];
                 int sum1 = value1 + value2;
+
+                if (value2 > 0 && sum1 >= target) break;
+
                 int p1 = j + 1;
                 int p2 = size - 1;
+                if (sum1 + nums[j + 1] + nums[j + 2] > target) goto endj;
+                if (sum1 + nums[len + 1] + nums[len + 2] < target) goto endj;
                 while (p1 < p2) {
                     int sum = sum1 + nums[p1] + nums[p2];
                     if (sum == target) {
@@ -61,9 +45,11 @@ public:
                         --p2;
                     }
                 }
+                endj:
                 while (j < len1 && nums[j] == nums[j + 1]) ++j;
                 ++j;
             }
+            endi:
             while (i < len1 && nums[i] == nums[i + 1]) ++i;
             ++i;
         }
@@ -74,9 +60,9 @@ public:
 
 int main() {
     vector<int> nums = {
-        { -5,5,4,-3,0,0,4,-2 }
+        { -4,-3,-2,-1,0,0,1,2,3,4 }
     };
-    int target = 4;
+    int target = 0;
     Solution solution;
     solution.fourSum(nums, target);
 }
