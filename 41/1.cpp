5a5ac387e1ab79d20600af92513becf30d7e82d5
tangs <tangsong1988kaka@gmail.com>
2020-01-02 21:27:37 +0800

    answer problem 41.

---
+++ 41/1.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int size = nums.size();
+        if (!size) return 1;
+        bool tmp[size];
+        memset(tmp, 0, sizeof(tmp));
+        for (int num: nums) {
+            if (num > 0 && num <= size)
+                tmp[num - 1] = true;
+        }
+        for (int i = 0; i < size; ++i)
+            if(!tmp[i]) return i + 1;
+        return size + 1;
+    }
+};
