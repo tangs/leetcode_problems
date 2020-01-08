537dca2b75537977acbb707d2631d310a6dff585
tangs <tangsong1988kaka@gmail.com>
2020-01-08 11:57:13 +0800

    answer problem 37.

--- 37/1.cpp
+++ 37/1.cpp
@@ -1,137 +1,137 @@
 #include <vector>
 #include <stdlib.h>
+#include <stdio.h>
 #include <stack>
+
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c%c"
+#define BYTE_TO_BINARY(byte)  \
+  (byte & 0x100 ? '1' : '0'), \
+  (byte & 0x80 ? '1' : '0'), \
+  (byte & 0x40 ? '1' : '0'), \
+  (byte & 0x20 ? '1' : '0'), \
+  (byte & 0x10 ? '1' : '0'), \
+  (byte & 0x08 ? '1' : '0'), \
+  (byte & 0x04 ? '1' : '0'), \
+  (byte & 0x02 ? '1' : '0'), \
+  (byte & 0x01 ? '1' : '0') 
 
 using namespace std;
 
 class Solution {
-    inline bool deal(vector<vector<char>>& board, int x, int y, short& bits) {
-        char value = board[y][x];
-        if (value != '.') {
-            bits &= ~(1 << (value - '1'));
-            return true;
+    void printHex(short mayable[9][9]) {
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 9; ++j) {
+                printf(BYTE_TO_BINARY_PATTERN" ", BYTE_TO_BINARY(mayable[i][j]));
+            }
+            printf("\n");
         }
-        return false;
     }
 
-    inline short getMayableNum(vector<vector<char>>& board, int x, int y, char& num) {
-        if (board[y][x] != '.') {
-            num = 0;
-            return 0;
-        }
-        short bits = 0x1ff;
-        num = 9;
-        int r = y / 3;
-        int c = x / 3;
+    void updateCanable(vector<vector<char>>& board, short canable[9][9]) {
         for (int i = 0; i < 9; ++i) {
-            if (i == x) continue;
-            if (deal(board, i, y, bits) || 
-                deal(board, x, i, bits) ||
-                deal(board, c * 3 + (i % 3), r * 3 + i / 3, bits)) {
-                --num;
+            for (int j = 0; j < 9; ++j) {
+                if (board[i][j] == '.') {
+                    canable[i][j] = 0x1FF;
+                    int gr = i / 3;
+                    int gc = j / 3;
+                    for (int k = 0; k < 9; ++k) {
+                        int hor = board[i][k] - '1';
+                        int ver = board[k][j] - '1';
+
+                        int cgr = gr * 3 + k / 3;
+                        int cgc = gc * 3 + k % 3;
+                        int group = board[cgr][cgc] - '1';
+                        if (k != j && hor >= 0) {
+                            canable[i][j] &= ~(1 << hor);
+                        }
+                        if (k != i && ver >= 0) {
+                            canable[i][j] &= ~(1 << ver);
+                        }
+                        if (!(i == cgr && j == cgc) && group >= 0) {
+                            canable[i][j] &= ~(1 << group);
+                        }
+                    }
+                } else {
+                    canable[i][j] = 0;
+                }
             }
         }
-        if (!num) bits = -1;
+    }
+
+    short getCanable(vector<vector<char>>& board, int i, int j) {
+        short bits = 0x1FF;
+        int gr = i / 3;
+        int gc = j / 3;
+        for (int k = 0; k < 9; ++k) {
+            int hor = board[i][k] - '1';
+            int ver = board[k][j] - '1';
+
+            int cgr = gr * 3 + k / 3;
+            int cgc = gc * 3 + k % 3;
+            int group = board[cgr][cgc] - '1';
+            if (k != j && hor >= 0) {
+                bits &= ~(1 << hor);
+            }
+            if (k != i && ver >= 0) {
+                bits &= ~(1 << ver);
+            }
+            if (!(i == cgr && j == cgc) && group >= 0) {
+                bits &= ~(1 << group);
+            }
+        }
         return bits;
     }
 
-    inline int getFirst(short bits) {
-        for (int i = 0; i < 9; ++i) {
-            if (bits & (1 << i)) return i;
-        }
-        return -1;
-    }
-
-    inline bool deal2(vector<vector<char>>& board, short mayable[9][9], int x, int y) {
-        char num;
-        short bits = getMayableNum(board, x, y, num);
-        mayable[y][x] = bits;
-        if (num == 1) {
-            char value = getFirst(bits) + '1';
-            updateValue(board, mayable, x, y, value);
+    bool deal(vector<vector<char>>& board, 
+    vector<int>& idxs, int cur) {
+        if (cur == idxs.size()) {
             return true;
         }
-        return false;
-    }
-
-    void updateValue(vector<vector<char>>& board, short mayable[9][9], 
-      int x, int y, char value) {
-        board[y][x] = value;
-        char num;
-        for (int i = y - 1; i >= 0; --i) {
-            if (deal2(board, mayable, x, i)) break;
-        }
-        for (int i = x - 1; i >= 0; --i) {
-            if (deal2(board, mayable, i, y)) break;
-        }
-        int r = y / 3;
-        int c = x / 3;
-        int cur = (y - r) * 3 + c;
+        int idx = idxs[cur];
+        int r = idx >> 4;
+        int c = idx & 0x0f;
+        // int can = canable[r][c];
+        int can = getCanable(board, r, c);
+        if (!can) return false;
         for (int i = 0; i < 9; ++i) {
-            if (i == cur) continue;
-            if (deal2(board, mayable, r * 3 + (i % 3), c * 3 + i / 3)) break;
+            if (!(can & (1 << i))) continue;
+            // char old = board[r][c];
+            board[r][c] = i + '1';
+            if (deal(board, idxs, cur + 1)) return true;
+            // board[r][c] = old;
         }
-    }
-
-    inline bool updateMayable(vector<vector<char>>& board, short mayable[9][9]) {
-        char num;
-        bool ret = true;
-        for (int i = 0; i < 9; ++i) {
-            for (int j = 0; j < 9; ++j) {
-                short bits = getMayableNum(board, j, i, num);
-                mayable[i][j] = bits;
-                if (bits == -1) ret = false;
-                if (num == 1) {
-                    int value = getFirst(bits) + '1';
-                    updateValue(board, mayable, j, i, value);
-                }
-            }
-        }
-        return ret;
-    }
-
-    int check(vector<vector<char>>& board) {
-        int ret = 0;
-        for (int i = 0; i < 9; ++i) {
-            for (int j = 0; j < 9; ++j) {
-            }
-        }
-    }
-
-    bool deal3(vector<vector<char>>& board, short mayable[9][9], int x, int y, int least) {
-        if (!least) {
-            if (check(board) == 0) {
-                return true;
-            }
-            return false;
-        }
-        for (int i = y; i < 9; ++i) {
-            for (int j = x; j < 9; ++j) {
-                if (board[i][j] == '.') {
-                    int bits = mayable[i][j];
-                    for (int i = 0; i < 9; ++i) {
-                        if (bits & (1 << i)) {
-                            board[i][j] = i;
-                        }
-                    }
-                    board[i][j] = '.';
-                }
-            }
-        }
+        board[r][c] = '.';
         return false;
     }
 
 public:
     void solveSudoku(vector<vector<char>>& board) {
-        short mayable[9][9];
+        // short canable[9][9];
         // memset(mayable, 0, sizeof(mayable));
-        updateMayable(board, mayable);
-        return;
+        // updateCanable(board, canable);
+        vector<int> idxs;
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 9; ++j) {
+                if (board[i][j] == '.') idxs.push_back((i << 4) + j);
+            }
+        }
+        deal(board, idxs, 0);
+        // printHex(canable);
+        int a = 3;
     }
 };
 
 int main() {
     vector<vector<char>> vecs = {
+        // {'5','3','4','6','7','8','9','1','2'},
+        // {'6','7','2','1','9','5','3','4','8'},
+        // {'1','9','8','3','4','2','5','6','7'},
+        // {'8','5','9','7','6','1','4','2','3'},
+        // {'4','2','6','8','.','3','.','.','1'},
+        // {'7','1','3','.','2','.','.','.','6'},
+        // {'.','6','.','.','.','.','2','8','.'},
+        // {'.','.','.','4','1','9','.','.','5'},
+        // {'.','.','.','.','8','.','.','7','9'}
         {'5','3','.','.','7','.','.','.','.'},
         {'6','.','.','1','9','5','.','.','.'},
         {'.','9','8','.','.','.','.','6','.'},
