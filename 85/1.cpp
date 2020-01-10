3d484f12740125571ad9caef48327e6bbd5f8696
tangs <tangsong1988kaka@gmail.com>
2020-01-10 10:43:21 +0800

    answer problem 85

--- 85/1.cpp
+++ 85/1.cpp
@@ -23,41 +23,49 @@ public:
 
         int row = m + 1;
         int col = n + 1;
-        int height[row][col];
+        // int height[row][col];
         int left[row][col];
-        int right[row][col];
+        // int right[row][col];
 
-        memset(height, 0, sizeof(height));
+        // memset(height, 0, sizeof(height));
         memset(left, 0, sizeof(left));
-        memset(right, 0, sizeof(right));
+        // memset(right, 0, sizeof(right));
 
         int ret = 0;
         for (int i = 1; i < row; ++i) {
-            right[i][n] = matrix[i - 1][n - 1] - '0';
+            // right[i][n] = matrix[i - 1][n - 1] - '0';
             for (int j = 1; j < col; ++j) {
                 if (matrix[i - 1][j - 1] == '1') {
-                    height[i][j] = height[i - 1][j] + 1;
+                    // height[i][j] = height[i - 1][j] + 1;
                     left[i][j] = left[i][j - 1] + 1;
-                }
-            }
-            for (int j = n; j > 0; --j) {
-                if (matrix[i - 1][j - 1] == '1') {
-                    right[i][j] = right[i][j + 1] + 1;
 
-                    int max1 = 0;
-                    for (int k = 0; k < i; ++k) {
-                        int h = height[k][j];
-                        if (!h) break;
-                        int area = h * (left[k][j] + right[k][j] - 1);
-                        ret = max(ret, area);
+                    int maxWidth = left[i][j];
+                    int maxArea = 0;
+                    for (int k = i; k > 0; --k) {
+                        maxArea = max(maxArea, maxWidth * (i - k + 1));
+                        maxWidth = min(maxWidth, left[k - 1][j]);
                     }
+                    ret = max(ret, maxArea);
                 }
             }
+            // for (int j = n; j > 0; --j) {
+            //     if (matrix[i - 1][j - 1] == '1') {
+            //         right[i][j] = right[i][j + 1] + 1;
+
+            //         int max1 = 0;
+            //         for (int k = 0; k < i; ++k) {
+            //             int h = height[k][j];
+            //             if (!h) break;
+            //             int area = h * (left[k][j] + right[k][j] - 1);
+            //             ret = max(ret, area);
+            //         }
+            //     }
+            // }
         }
 
-        print2DArray((int*)height, row, col);
-        print2DArray((int*)left, row, col);
-        print2DArray((int*)right, row, col);
+        // print2DArray((int*)height, row, col);
+        // print2DArray((int*)left, row, col);
+        // print2DArray((int*)right, row, col);
 
         return ret;
     }
