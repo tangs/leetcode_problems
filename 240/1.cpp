a45631c6145e5344bf1b895aaa3ddc9fdf5247a2
tangs <tangsong1988kaka@gmail.com>
2020-01-14 11:00:17 +0800

    update 240 problem answer.

--- 240/1.cpp
+++ 240/1.cpp
@@ -12,62 +12,28 @@ public:
         int n = matrix[0].size();
         if (!n) return false;
 
-        int len = m * n;
-        int i = m >> 1;
-        int j = n >> 1;
-        for (;;) {
-            int v = matrix[i][j];
-            if (v == target) return true;
-            bool isEndI = i == m - 1;
-            bool isEndJ = j == n - 1;
-            // if (isEndI && isEndJ) return false;
-            if (v < target) {
-                if (isEndI && isEndJ) return false;
-                if (isEndI) {
-                    if (matrix[i][j + 1] > target) return false;
-                    ++j;
-                } else if (isEndJ) {
-                    if (matrix[i + 1][j] > target) return false;
-                    ++i;
-                } else {
-                    int num1 = target - matrix[i][j + 1];
-                    int num2 = target - matrix[i + 1][j];
-                    if (!num1 || !num2) return true;
-                    if (num1 < 0 && num2 < 0) return false;
-                    if (num1 > 0 && num2 > 0) {
-                        ++i;
-                        ++j;
-                    } else if (num1 > 0) ++j;
-                    else ++i;
-                }
-            } else {
-                if (i == 0 && j == 0) return false;
-                if (!i) { 
-                    if (matrix[i][j - 1] < target) return false;
-                    --j;
-                } else if (!j) {
-                    if (matrix[i - 1][j] < target) return false;
-                     --i;
-                } else {
-                    int num1 = matrix[i][j - 1] - target;
-                    int num2 = matrix[i - 1][j] - target;
-                    if (!num1 || !num2) return true;
-                    if (num1 < 0 && num2 < 0) return false;
-                    if (num1 > 0 && num2 > 0) {
-                        --i;
-                        --j;
-                    } else if (num1 > 0) --j;
-                    else --i;
-                }
-            }
+        int y = m - 1;
+        int x = 0;
+
+        while (x < n && y >= 0) {
+            if (matrix[y][x] == target) return true;
+            if (matrix[y][x] > target) --y;
+            else ++x;
         }
+
+        return false;
     }
 };
 
 int main() {
     vector<vector<int>> matrix = {
-        {1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25}
+        // {1,2,3,4,5},
+        // {6,7,8,9,10},
+        // {11,12,13,14,15},
+        // {16,17,18,19,20},
+        // {21,22,23,24,25}
+        { -1, 3 }
     };
     Solution solu;
-    solu.searchMatrix(matrix, 5);
+    solu.searchMatrix(matrix, 1);
 }
