609b4727e719e27c91fc3b91a5c635d958f7bf38
tangs <tangsong1988kaka@gmail.com>
2020-01-14 22:09:02 +0800

    answer problem 76.

--- 76/1.cpp
+++ 76/1.cpp
@@ -6,33 +6,50 @@
 using namespace std;
 
 class Solution {
+    inline bool isOk(int* nums1, int* nums2) {
+        for (int i = 0; i < 128; ++i) {
+            if (nums1[i] && nums1[i] > nums2[i]) return false;
+        }
+        return true;
+    }
 public:
     string minWindow(string s, string t) {
-        long bits = 0;
+        int tlen = t.size();
+        // long bits = 0;
+        int destNums[128];
+        memset(destNums, 0, sizeof(destNums));
         for (char ch: t) {
-            int num = ch >= 'a' ? ch - 'a' + 32 : ch - 'A';
-            bits |= 1l << num;
+            ++destNums[ch];
         }
 
         int len = s.size();
-        long dp[len + 1];
-        // memset(dp, 0, sizeof(dp));
+
         int min = INT_MAX;
         int dests = -1;
-        for (int i = 0; i < len; ++i) {
-            dp[i] = 0;
-            for (int j = i; j < len; ++j) {
-                // long bit = s[j] - 'A';
-                char ch = s[j];
-                int num = ch >= 'a' ? ch - 'a' + 32 : ch - 'A';
-                dp[j + 1] = dp[j] | (1l << num);
-                int len = j - i + 1;
-                if (len < min && (bits ^ dp[j + 1] & bits) == 0) {
-                    min = len;
-                    dests = i;
+        int nums[128];
+        memset(nums, 0, sizeof(nums));
+
+        int i = 0;
+        for (int j = 0; j < len; ++j) {
+            ++nums[s[j]];
+            if (isOk(destNums, nums)) {
+                // int len1 = j - i + 1;
+                for (int len2 = j - i + 1; len2 >= tlen; --len2) {
+                    if (!isOk(destNums, nums)) {
+                        break;
+                    }
+                    if (len2 == tlen) {
+                        return s.substr(i, len2);
+                    }
+                    if (len2 < min) {
+                        min = len2;
+                        dests = i;
+                    }
+                    --nums[s[i++]];
                 }
             }
         }
+        // }
         if (dests == -1) return "";
         else return s.substr(dests, min);
     }
@@ -40,5 +57,8 @@ public:
 
 int main() {
     Solution solu;
-    solu.minWindow("ADOBECODEBANC", "ABC");
+    solu.minWindow(
+        "aaaaaaaaaaaabbbbbcdd",
+        "abcdd"
+    );
 }
