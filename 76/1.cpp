76a97e8172968fd7e2d632f31993420e5f14572e
tangs <tangsong1988kaka@gmail.com>
2020-01-15 10:39:59 +0800

    update 76.

--- 76/1.cpp
+++ 76/1.cpp
@@ -6,56 +6,33 @@
 using namespace std;
 
 class Solution {
-    inline int getIdx(char ch) {
-        return ch >= 'a' ? ch - 'a' + 26 : ch - 'A';
-    }
-    inline bool isOk(int* nums1, int* nums2) {
-        for (int i = 0; i < 52; ++i) {
-            if (nums1[i] && nums1[i] > nums2[i]) return false;
-        }
-        return true;
-    }
 public:
     string minWindow(string s, string t) {
         int tlen = t.size();
-        int destNums[52];
-        memset(destNums, 0, sizeof(destNums));
-        for (char ch: t) {
-            ++destNums[getIdx(ch)];
-        }
-
         int len = s.size();
 
-        int min = INT_MAX;
-        int dests = -1;
-        int nums[52];
-        memset(nums, 0, sizeof(nums));
+        int destNums[128] = {0};
+        for (char ch: t) ++destNums[ch];
 
-        int i = 0;
-        int cnt;
-        for (int j = 0; j < len; ++j) {
-            int idx1 = getIdx(s[j]);
-            ++nums[idx1];
-            if (destNums[idx1] > 0) ++cnt; else continue;
-            if (cnt >= tlen && isOk(destNums, nums)) {
-                int len1 = j - i + 1;
-                for (int len2 = len1; len2 >= tlen; ) {
-                    if (len2 == tlen) {
-                        return s.substr(i, len2);
-                    }
-                    if (len2 < min) {
-                        min = len2;
-                        dests = i;
-                    }
-                    char ch = s[i++];
-                    int idx = getIdx(ch);
-                    if (destNums[idx] > 0) --cnt;
-                    --nums[idx];
-                    --len2;
-                    if (nums[idx] < destNums[idx]) {
-                        break;
-                    }
+        int min = len + 1;
+        int dests = -1;
+
+        int cnt = 0;
+        for (int left = 0, right = 0; right < len; ++right) {
+            // int idx1 = getIdx(s[right]);
+            if (--destNums[s[right]] >= 0) ++cnt; else continue;
+            while (cnt == tlen) {
+                int len2 = right - left + 1;
+                if (len2 == tlen) {
+                    return s.substr(left, len2);
                 }
+                if (len2 < min) {
+                    min = len2;
+                    dests = left;
+                }
+                // int idx = getIdx(s[left++]);
+                --len2;
+                if (++destNums[s[left++]] > 0) --cnt;
             }
         }
         if (dests == -1) return "";
@@ -66,7 +43,7 @@ public:
 int main() {
     Solution solu;
     solu.minWindow(
-        "aaaaaaaaaaaabbbbbcdd",
-        "abcdd"
+        "ADOBECODEBANC",
+        "ABC"
     );
 }
