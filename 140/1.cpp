3f22ae044d18fdb37c767cf37602c91ebc97876d
tangs <tangsong1988kaka@gmail.com>
2020-02-26 19:00:39 +0800

    updtae.

--- 140/1.cpp
+++ 140/1.cpp
@@ -15,48 +15,26 @@ public:
         
         unordered_map<int, unordered_set<string>> wordMap;
 
-        bool dp[len][len];
-        for (int i = 0; i < len; ++i) {
-            string str = s.substr(i, 1);
-            dp[i][i] = wordSet.find(str) != wordSet.end();
-            if (dp[i][i]) {
-                wordMap[(i << 16) | i].insert(move(str));
+        int dp[len + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= len; ++i) {
+            dp[i] = 0;
+            string str = s.substr(0, i);
+            if (wordSet.find(str) != wordSet.end()) {
+                wordMap[i].insert(move(str));
+                dp[i] = 1;
+            }
+            for (int j = 1; j < i; ++j) {
+                if (!dp[j]) continue;
+                string str = s.substr(j, i - j);
+                if (wordSet.find(str) == wordSet.end()) continue;
+                string str1 = s.substr(0, j) + " " + str;
+                wordMap[i].insert(move(str1));
+                dp[i] = 1;
             }
         }
-
-        for (int i = 1; i < len; ++i) {
-            for (int j = 0; j < len - i; ++j) {
-                int col = i + j;
-                dp[j][col] = 0;
-                int idx = (j << 16) | col;
-                unordered_set<string> strs;
-                for (int k = 0; k < i; ++k) {
-                    if (dp[j][j + k] && dp[j + k + 1][col]) {
-                        dp[j][col] = 1;
-                        int idx1 = (j << 16) | (j + k);
-                        int idx2 = ((j + k + 1) << 16) | col;
-                        auto& vec1 = wordMap[idx1];
-                        auto& vec2 = wordMap[idx2];
-                        for (auto str1: vec1) {
-                            string str;
-                            for (auto str2: vec2) {
-                                str = str1 + " " + str2;
-                                strs.insert(move(str));
-                            }
-                        }
-                    }
-                }
-                string str = s.substr(j, i + 1);
-                if (wordSet.find(str) != wordSet.end()) {
-                    // wordMap[idx].push_back(move(str));
-                    strs.insert(move(str));
-                    dp[j][col] = 1;
-                }
-                wordMap[idx] = move(strs);
-            }
-        }
-        auto& lastStrs = wordMap[len - 1];
-        return vector<string>(lastStrs.begin(), lastStrs.end());
+        auto& last = wordMap[len];
+        return vector<string>(last.begin(), last.end());
     }
 };
 
