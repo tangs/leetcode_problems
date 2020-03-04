ceb18c62e3ed11bbea03ac889b04b9f206cf9594
tangs <tangsong1988kaka@gmail.com>
2020-03-04 11:00:38 +0800

    update problem 207 answer.

--- 207/1.cpp
+++ 207/1.cpp
@@ -15,41 +15,21 @@ public:
             adj[arr[1]].push_back(arr[0]);
 
         int color[numCourses];
-        // int b[numCourses];
-        // int f[numCourses];
-        int pi[numCourses];
-        // int time = 0;
-
-        memset(pi, -1, sizeof(pi));
         memset(color, 0, sizeof(color));
 
         std::function<bool(int)> dfs_visit;
         dfs_visit = [&] (int idx) {
             color[idx] = 1;
             for (int idx1: adj[idx]) {
-                pi[idx1] = idx;
                 if (color[idx1] == 1) return false;
-                if (color[idx1]) continue;
-                if (!dfs_visit(idx1)) return false;
+                if (!color[idx1] && !dfs_visit(idx1)) return false;
             }
             color[idx] = 2;
             return true;
         };
-
-        for (int i = 0; i < numCourses; ++i) {
-            if (color[i]) continue;
-            // pi[i] = -1;
-            if (!dfs_visit(i)) return false; 
-        }
-
-        unordered_set<int> heads;
-        for (int i = 0; i < numCourses; ++i) if (pi[i] == -1) heads.insert(i);
-        if (!heads.size()) return false;
-
-        memset(color, 0, sizeof(color));
-        for (int i: heads) {
-            if (!dfs_visit(i)) return false; 
-        }
+        
+        for (int i = 0; i < numCourses; ++i)
+            if (!color[i] && !dfs_visit(i)) return false;
 
         return true;
     }
