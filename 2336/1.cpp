272c3e5759faaed3f96ea67d40032e8fe111fa74
tangs <tangsong1988kaka@gmail.com>
2023-11-29 15:13:00 +0800

    add 2336 problem.

---
+++ 2336/1.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <string>
+#include <list>
+#include <set>
+
+class SmallestInfiniteSet {
+  std::set <int> nums;
+  int next = 1;
+public:
+  SmallestInfiniteSet() = default;
+
+  int popSmallest() {
+    if (nums.empty())  return next++;
+    const auto it = nums.cbegin();
+    const auto ret = *it;
+    nums.erase(it);
+    return ret;
+  }
+
+  void addBack(const int num) {
+    if (num >= next) return;
+    nums.insert(num);
+  }
+};
+
+int main() {
+
+}
