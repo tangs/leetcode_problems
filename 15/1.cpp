de7bbc4f4e0816d73a8e33444006e5140706fc3a
tangs <tangsong1988kaka@gmail.com>
2023-11-30 18:52:09 +0800

    add problem 15.

---
+++ 15/1.cpp
@@ -0,0 +1,37 @@
+#include <vector>
+#include <unordered_set>
+
+class Solution {
+public:
+  std::vector<std::vector<int>> threeSum(const std::vector<int>& nums) {
+    const auto n = nums.size();
+    std::sort(nums.begin(), nums.end());
+
+    std::vector<std::vector<int>> ret;
+    for (int i = 0; i < n - 2;) {
+      int l = i + 1;
+      int r = n - 1;
+      const auto num = nums[i];
+      while (l < r) {
+        const int sum = num + nums[l] + nums[r];
+        if (sum > 0) {
+          while (--r > l && nums[r] == nums[r + 1]);
+          continue;
+        }
+        if (sum == 0) {
+          ret.push_back({nums[i], nums[l], nums[r]});
+        }
+        while (++l < r && nums[l] == nums[l - 1]);
+      }
+      while (++i < n && nums[i] == nums[i - 1]);
+    }
+    return ret;
+  }
+};
+
+int main() {
+  std::vector nums {-1,0,1,2,-1,-4};
+  Solution solution;
+  auto ret = solution.threeSum(nums);
+  return 0;
+}
