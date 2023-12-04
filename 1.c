3dd7762e38f4a124e1b4b0351129c0f3d20199c1
tangs <tangsong1988kaka@gmail.com>
2023-12-04 11:32:59 +0800

    refactor.

--- 1.c
+++
@@ -1,9 +0,0 @@
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-    int a = 0x80000000;
-    printf("%d\n", a);
-    printf("%d\n", abs(a));
-}
--- compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-g++ -g -O0 -std=c++17 -o main $1/1.cpp
