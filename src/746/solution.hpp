5ea7ef3b63792828342fc8d9c8f47ca5488f2067
tangs <tangsong1988kaka@gmail.com>
2023-12-25 15:01:48 +0800

    add 746 solution.

---
+++ src/746/solution.hpp
@@ -0,0 +1,17 @@
+#include <vector>
+
+class Solution {
+public:
+  int minCostClimbingStairs(std::vector<int> cost) {
+    const auto n = cost.size();
+    int prev = 0, cur = 0;
+
+    for (auto i = 0; i < n; ++i) {
+      int tmp = cost[i] + std::min(cur, prev);
+      prev = cur;
+      cur = tmp;
+    }
+
+    return std::min(prev, cur);
+  }
+};
