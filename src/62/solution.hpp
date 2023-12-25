81912d4266e9b990bb7d035779e8005477d6a747
tangs <tangsong1988kaka@gmail.com>
2023-12-25 18:18:40 +0800

    refactor.

--- src/62/solution.hpp
+++ src/62/solution.hpp
@@ -3,15 +3,14 @@
 class Solution {
 public:
   int uniquePaths(int m, int n) {
-    std::vector<int> dp(n + 1, 1);
-    dp[0] = 0;
+    std::vector<int> dp(n, 1);
 
     for (int i = 1; i < m; ++i) {
-      for (int j = 1; j <= n; ++j) {
+      for (int j = 1; j < n; ++j) {
         dp[j] += dp[j - 1];
       }
     }
 
-    return dp[n];
+    return dp[n - 1];
   }
 };
