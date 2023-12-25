7c5c597d55a0cbb62ade25068c16777324cfb036
tangs <tangsong1988kaka@gmail.com>
2023-12-25 16:10:38 +0800

    add 740 solution.

---
+++ src/740/solution.hpp
@@ -0,0 +1,8 @@
+#include <vector>
+
+class Solution {
+public:
+  int deleteAndEarn(const std::vector<int>& nums) {
+
+  }
+};
---
+++ tests/740/test.cc
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+
+#include "solution.hpp"
+
+TEST(testing, BasicAssertions) {
+
+}
