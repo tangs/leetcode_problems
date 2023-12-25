72d607de9e914cebe1910fc23555f42d6b38e10c
tangs <tangsong1988kaka@gmail.com>
2023-12-25 17:19:40 +0800

    add 740 unit test.

--- tests/740/test.cc
+++ tests/740/test.cc
@@ -2,8 +2,11 @@
 
 #include "solution.hpp"
 
-TEST(testing, BasicAssertions) {
+TEST(delete_and_earn_exp_testing, BasicAssertions) {
   Solution solution;
 
+  EXPECT_EQ(solution.deleteAndEarn({3,4,2}), 6);
+  EXPECT_EQ(solution.deleteAndEarn({2,2,3,3,3,4}), 9);
   EXPECT_EQ(solution.deleteAndEarn({134,5,6,34,33,32,53,33,32,33,35,64,742,46,4646,4445,777,8756}), 19803);
+  EXPECT_EQ(solution.deleteAndEarn({134,5,6,34,33,9999,53,10000,32,33,35,9999,10000,46,4646,4445,777,8756}), 38964);
 }
