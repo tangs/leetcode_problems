9f158d437b391a8e492918ccca2efd55d70cee12
tangs <tangsong1988kaka@gmail.com>
2023-12-25 17:06:03 +0800

    implement 740 solution.

--- src/740/solution.hpp
+++ src/740/solution.hpp
@@ -1,8 +1,22 @@
 #include <vector>
+#include <array>
 
 class Solution {
 public:
   int deleteAndEarn(const std::vector<int>& nums) {
+    constexpr int CNT = 10001;
+    std::array<int, CNT> times{};
 
+    for (auto num: nums) ++times[num];
+
+    int prev = 0, cur = 0;
+
+    for (int i = 0; i < CNT; ++i) {
+      int tmp = std::max(cur, prev + i * times[i]);
+      prev = cur;
+      cur = tmp;
+    }
+
+    return cur;
   }
 };
--- tests/740/test.cc
+++ tests/740/test.cc
@@ -3,5 +3,7 @@
 #include "solution.hpp"
 
 TEST(testing, BasicAssertions) {
+  Solution solution;
 
+  EXPECT_EQ(solution.deleteAndEarn({134,5,6,34,33,32,53,33,32,33,35,64,742,46,4646,4445,777,8756}), 19803);
 }
