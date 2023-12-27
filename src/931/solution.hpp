8dd8866f46e27402831626a17b232fcc21d036c5
tangs <tangsong1988kaka@gmail.com>
2023-12-27 15:24:55 +0800

    optimize.

--- src/931/solution.hpp
+++ src/931/solution.hpp
@@ -5,12 +5,13 @@ public:
   int minFallingPathSum(const std::vector<std::vector<int>>& matrix) {
     const auto n = matrix.size();
     std::vector<int> dp = matrix[0];
+    std::vector<int> prevDp(n);
 
     for (int i = 1; i < n; ++i) {
-      auto prevDp = dp;
+      std::swap(dp, prevDp);
       dp[0] = std::min(prevDp[0], prevDp[1]) + matrix[i][0];
       for (int j = 1; j < n - 1; ++j) {
-        dp[j] = *std::min_element(prevDp.cbegin() + j - 1, prevDp.cbegin() + j + 2) + matrix[i][j];
+        dp[j] = std::min({prevDp[j - 1], prevDp[j], prevDp[j + 1]}) + matrix[i][j];
       }
       dp[n - 1] = std::min(prevDp[n - 2], prevDp[n - 1]) + matrix[i][n - 1];
     }
